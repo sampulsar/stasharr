431d73ff2b29bd1dfede931906b4ddd85a35e9a4
enymawse <enymawse@pm.me>
2024-11-14 14:00:23 -0800

    put stasharr settings link in stash navbar

---
+++ src/controller/NavbarController.ts
@@ -0,0 +1,39 @@
+import { Stasharr } from "../enums/Stasharr";
+
+export class NavbarController {
+  private observer: MutationObserver = new MutationObserver((mutationList) => {
+    for (const mutationRecord of mutationList) {
+      if (
+        mutationRecord.type === "childList" &&
+        mutationRecord.addedNodes.length > 0
+      ) {
+        mutationRecord.addedNodes.forEach((node) => {
+          if (node instanceof HTMLElement) {
+            if (node.querySelector("nav.navbar > .navbar-nav")) {
+              const navbar = document.querySelector("nav.navbar > .navbar-nav");
+              if (navbar) {
+                navbar.appendChild(this.createSettingsModalLink());
+                this.observer.disconnect();
+                return;
+              }
+            }
+          }
+        });
+      }
+    }
+  });
+
+  private createSettingsModalLink(): HTMLElement {
+    let aLink = document.createElement("a");
+    aLink.classList.add("nav-link");
+    aLink.href = "#";
+    aLink.innerText = "Stasharr";
+    aLink.setAttribute("data-bs-toggle", "modal");
+    aLink.setAttribute("data-bs-target", Stasharr.DOMSelector.SettingsModal);
+    return aLink;
+  }
+
+  constructor(body: HTMLElement) {
+    this.observer.observe(body, { childList: true, subtree: true });
+  }
+}
--- src/enums/Stasharr.ts
+++ src/enums/Stasharr.ts
@@ -8,12 +8,14 @@ export namespace Stasharr {
   export enum ID {
     CardButton = "stasharr-button",
     HeaderButton = "stasharr-header-button",
+    SettingsModal = "stasharr-settingsModal",
     AddAllAvailable = "stasharr-addallavailable",
     SearchAllExisting = "stasharr-searchallavailable",
   }
   export class DOMSelector {
     static CardButton = `#${ID.CardButton}`;
     static HeaderButton = `#${ID.HeaderButton}`;
+    static SettingsModal = `#${ID.SettingsModal}`;
     static AddAllAvailable = `#${ID.AddAllAvailable}`;
     static SearchAllExisting = `#${ID.SearchAllExisting}`;
     static SceneCardByButtonStatus = (status: SceneStatus) => {
